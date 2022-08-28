082eb3eba581054c7db711d7e66b5592419b77bf
Камельчуков Александр <lexkml@yandex.ru>
2022-08-28 12:03:10 +0300

    Lesson-9. Bean scopes.

--- src/main/java/ru/lexkml/spring/ApplicationRunner.java
+++ src/main/java/ru/lexkml/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package ru.lexkml.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.lexkml.spring.database.pool.ConnectionPool;
 import ru.lexkml.spring.database.repository.CompanyRepository;
 
 public class ApplicationRunner {
@@ -8,6 +9,10 @@ public class ApplicationRunner {
         var context = new ClassPathXmlApplicationContext("application.xml");
 
         var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        
+        var connectionPoolBean = context.getBean("connectionPoolBean", ConnectionPool.class);
+        var connectionPoolBean2 = context.getBean("connectionPoolBean", ConnectionPool.class);
+        var connectionPoolBean3 = context.getBean("connectionPoolBean", ConnectionPool.class);
 
         System.out.println();
     }
