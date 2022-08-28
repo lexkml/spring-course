9b6b5ac7eae1c74b3940a6b1d9ef4fc436d841dc
Alexandr Kamelchukov <lexkml@yandex.ru>
2022-08-28 15:47:46 +0300

    Lesson-14. Annotation based configuration.

--- src/main/java/ru/lexkml/spring/database/pool/ConnectionPool.java
+++ src/main/java/ru/lexkml/spring/database/pool/ConnectionPool.java
@@ -1,5 +1,8 @@
 package ru.lexkml.spring.database.pool;
 
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +18,11 @@ public class ConnectionPool {
         this.arguments = arguments;
         this.properties = properties;
     }
-
+    @PostConstruct
     private void init() {
         System.out.println("Initialization connection pool in initialization method");
     }
-
+    @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool in destroy method");
     }
