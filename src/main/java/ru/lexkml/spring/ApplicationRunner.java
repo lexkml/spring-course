3e7ab5347f3ee91a3f4cf149b2d2d17cfb01a089
Камельчуков Александр <lexkml@yandex.ru>
2022-08-28 11:46:39 +0300

    Lesson-7. Factory method injection.

--- src/main/java/ru/lexkml/spring/ApplicationRunner.java
+++ src/main/java/ru/lexkml/spring/ApplicationRunner.java
@@ -1,15 +1,14 @@
 package ru.lexkml.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.lexkml.spring.database.pool.ConnectionPool;
+import ru.lexkml.spring.database.repository.CompanyRepository;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
 
-        var connectionPool1 = context.getBean("pool", ConnectionPool.class);
-        var someBean = context.getBean("someBean", String.class);
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
 
-        System.out.println(connectionPool1);
+        System.out.println();
     }
 }
--- src/main/java/ru/lexkml/spring/database/pool/ConnectionPool.java
+++ src/main/java/ru/lexkml/spring/database/pool/ConnectionPool.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 public class ConnectionPool {
-    private final String username;
+    private final String user;
     private final Integer poolSize;
     private final List<Object> arguments;
     private final Map<String, Object> properties;
 
-    public ConnectionPool(String username, Integer poolSize, List<Object> arguments, Map<String, Object> properties) {
-        this.username = username;
+    public ConnectionPool(String user, Integer poolSize, List<Object> arguments, Map<String, Object> properties) {
+        this.user = user;
         this.poolSize = poolSize;
         this.arguments = arguments;
         this.properties = properties;
