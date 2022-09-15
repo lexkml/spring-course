19fefa6c1fa98e8e20df850dd89a1cc18a84843b
Камельчуков Александр Александрович <kamelchukov@exportcenter.ru>
2022-09-15 15:39:22 +0300

    Lessons 29-30. Event listeners. Part 1-2

--- src/main/java/ru/lexkml/spring/service/CompanyService.java
+++ src/main/java/ru/lexkml/spring/service/CompanyService.java
@@ -1,13 +1,33 @@
 package ru.lexkml.spring.service;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import ru.lexkml.spring.database.entity.Company;
+import ru.lexkml.spring.database.repository.CrudRepository;
+import ru.lexkml.spring.dto.CompanyReadDto;
+import ru.lexkml.spring.listener.entity.AccessType;
+import ru.lexkml.spring.listener.entity.EntityEvent;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
 
+    private final CrudRepository<Integer, Company> companyRepository;
     private final UserService userService;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public CompanyService(UserService userService) {
+    public CompanyService(CrudRepository<Integer, Company> companyRepository, UserService userService, ApplicationEventPublisher eventPublisher) {
+        this.companyRepository = companyRepository;
         this.userService = userService;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public Optional<CompanyReadDto> findById(Integer id) {
+        return companyRepository.findById(id)
+                .map(company -> {
+                    eventPublisher.publishEvent(new EntityEvent(company, AccessType.READ));
+                    return new CompanyReadDto(company.id());
+                });
     }
 }
