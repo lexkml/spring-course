920b77864b757c44cead5a74647588c9537a0bcb
Камельчуков Александр Александрович <kamelchukov@exportcenter.ru>
2022-09-15 10:19:40 +0300

    Lesson 24. Java-based Configuration

--- src/main/java/ru/lexkml/spring/ApplicationRunner.java
+++ src/main/java/ru/lexkml/spring/ApplicationRunner.java
@@ -1,11 +1,12 @@
 package ru.lexkml.spring;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.lexkml.spring.config.ApplicationConfiguration;
 import ru.lexkml.spring.database.repository.CrudRepository;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
             var companyRepository = context.getBean("companyRepository", CrudRepository.class);
             var maybeCompany = companyRepository.findById(1);
 
---
+++ src/main/java/ru/lexkml/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package ru.lexkml.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "ru.lexkml.spring")
+public class ApplicationConfiguration {
+
+
+
+}
