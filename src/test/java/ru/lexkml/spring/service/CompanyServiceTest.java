98bc3e87d7e0f3502b2f83783185a303d54bae4c
Alexandr Kamelchukov <lexkml@yandex.ru>
2022-09-16 13:56:41 +0300

    Lesson 41. Test starter

---
+++ src/test/java/ru/lexkml/spring/service/CompanyServiceTest.java
@@ -0,0 +1,49 @@
+package ru.lexkml.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+import ru.lexkml.spring.database.entity.Company;
+import ru.lexkml.spring.database.repository.CrudRepository;
+import ru.lexkml.spring.dto.CompanyReadDto;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+    private static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private CrudRepository<Integer, Company> companyRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        doReturn(Optional.of(new Company(COMPANY_ID)))
+                .when(companyRepository).findById(COMPANY_ID);
+
+        var actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> {
+            assertEquals(expectedResult, actual);
+        });
+
+//        verify(eventPublisher).publishEvent(any(EntityEvent.class));
+    }
+}
