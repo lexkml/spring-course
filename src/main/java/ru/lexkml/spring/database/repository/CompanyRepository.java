21d028fa3e77cd4407a39d9ff90535c22e32923e
Alexandr Kamelchukov <kamelchukov@exportcenter.ru>
2022-11-16 09:16:13 +0300

    Lesson 54. RepositoryQuery

--- src/main/java/ru/lexkml/spring/database/repository/CompanyRepository.java
+++ src/main/java/ru/lexkml/spring/database/repository/CompanyRepository.java
@@ -1,12 +1,12 @@
 package ru.lexkml.spring.database.repository;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import ru.lexkml.spring.database.entity.Company;
 
 import java.util.Optional;
 
 
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
     Optional<Company> findById(Integer id);
     void delete(Company entity);
 }
