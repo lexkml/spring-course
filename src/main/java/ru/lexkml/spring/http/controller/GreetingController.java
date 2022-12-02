782d1366539da75de2b1c5545b9afdf0cb79965c
Alexandr Kamelchukov <kamelchukov@exportcenter.ru>
2022-12-02 14:41:12 +0300

    Lesson 78. Parameters, Headers, Cookies

--- src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
+++ src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
@@ -1,19 +1,30 @@
 package ru.lexkml.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable Integer id
+    ) {
+        var ageParamValue = request.getParameter("age");
+        var acceptHeader = request.getHeader("accept");
+        var cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
+
     @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
