d6e2edf660f2d5f2e4c72ac5fafd4355a044d171
Alexandr Kamelchukov <kamelchukov@exportcenter.ru>
2022-12-02 14:25:51 +0300

    Lesson 77. @RequestMapping

--- src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
+++ src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
@@ -1,22 +1,22 @@
 package ru.lexkml.spring.http.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
-
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
-
-
 }
