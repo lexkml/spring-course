3fa5679bed5d19f426031d65b3f0887f5098784d
Alexandr Kamelchukov <kamelchukov@exportcenter.ru>
2022-12-02 13:25:03 +0300

    Lesson 72-73. Liquibase

--- src/main/java/ru/lexkml/spring/database/repository/FilterUserRepository.java
+++ src/main/java/ru/lexkml/spring/database/repository/FilterUserRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 public interface FilterUserRepository {
 
-    List<User> findAllByFilter(UserFilter filter);
+//    List<User> findAllByFilter(UserFilter filter);
+//
+//    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
+//
+//    void updateCompanyAndRole(List<User> users);
 
-    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
-
-    void updateCompanyAndRole(List<User> users);
-
-    void updateCompanyAndRoleNamed(List<User> users);
+//    void updateCompanyAndRoleNamed(List<User> users);
 }
