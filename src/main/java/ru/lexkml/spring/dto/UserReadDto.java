e1d0f8316be50d8dac276f8ae3fd31af995117c1
Alexandr Kamelchukov <kamelchukov@exportcenter.ru>
2022-12-02 14:52:49 +0300

    Lesson 79. Model

---
+++ src/main/java/ru/lexkml/spring/dto/UserReadDto.java
@@ -0,0 +1,9 @@
+package ru.lexkml.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+    Long id;
+    String username;
+}
--- src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
+++ src/main/java/ru/lexkml/spring/http/controller/GreetingController.java
@@ -3,20 +3,30 @@ package ru.lexkml.spring.http.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import ru.lexkml.spring.dto.UserReadDto;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
+    @GetMapping("/hello")
+
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+        return modelAndView;
+    }
+
     @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView,
-                              HttpServletRequest request,
-                              @RequestParam Integer age,
-                              @RequestHeader String accept,
-                              @CookieValue("JSESSIONID") String jsessionId,
-                              @PathVariable Integer id
+    public ModelAndView hello2(ModelAndView modelAndView,
+                               HttpServletRequest request,
+                               @RequestParam Integer age,
+                               @RequestHeader String accept,
+                               @CookieValue("JSESSIONID") String jsessionId,
+                               @PathVariable Integer id
     ) {
         var ageParamValue = request.getParameter("age");
         var acceptHeader = request.getHeader("accept");
@@ -26,7 +36,7 @@ public class GreetingController {
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(ModelAndView modelAndView, @SessionAttribute("user") UserReadDto userReadDto) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
